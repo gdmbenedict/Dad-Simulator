8199d17541502e6f1bd04c29e44a0d58f05d8d0f
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-01-26 21:35:42 -0400

    Require E held to move mower.

--- Assets/MowerController.cs
+++ Assets/MowerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,6 +13,7 @@ public class MowerController : MonoBehaviour
     // Movement backend things
     Vector2 moveValue;
     Rigidbody rb;
+    [SerializeField] bool move = true;
 
 
     void Start()
@@ -29,15 +31,23 @@ public class MowerController : MonoBehaviour
             transform.Rotate(rot);
 
             // Movement
-            Vector3 moveVector = transform.forward * moveSpeed * Time.deltaTime;
-            if(moveValue.y < 0) { moveVector *= -0.5f; } // If we want to reverse, allow that, but make it slow.
-            Vector3 movePos = moveVector + transform.position;
-            rb.MovePosition(movePos);
+            if (move)
+            {
+                Vector3 moveVector = transform.forward * moveSpeed * Time.deltaTime;
+                if (moveValue.y < 0) { moveVector *= -0.5f; } // If we want to reverse, allow that, but make it slow.
+                Vector3 movePos = moveVector + transform.position;
+                rb.MovePosition(movePos);
+            }
         }
     }
 
     void OnMove(InputValue value)
     {
         moveValue = value.Get<Vector2>();
+    }
+
+    void OnActivate(InputValue value)
+    {
+        move = Convert.ToBoolean(value.Get<float>());
     }
 }
