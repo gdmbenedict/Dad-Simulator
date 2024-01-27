34398d08e3b042dc3d175cb04a387f3ea5cfbace
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-01-27 13:23:07 -0400

    Add text things.
    Also set some default values.

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -1,24 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] float timer;
-    [SerializeField] bool active;
-    [SerializeField] int loseSceneID;
+    // Serialized
+    [SerializeField] float timer = 300;
+    [SerializeField] bool active = false;
+    [SerializeField] int loseSceneID = 2;
+
+    // Backend
+    TextMeshProUGUI timerText;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Get timer text component from children. Please use prefab if able.
+        timerText = GetComponentInChildren<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(active)
+        // Only do things if timer is supposed to be active.
+        if (active)
         {
             timer -= Time.deltaTime;
 
@@ -28,6 +33,8 @@ public class Timer : MonoBehaviour
                 OnLose();
             }
         }
+
+        SetTimerText();
     }
 
     public void SetTimer(float time) // set timer to whatever time we tell it, if for some reason we need to change it.
@@ -43,5 +50,23 @@ public class Timer : MonoBehaviour
     private void OnLose() // if we lose, load the lose scene. We can change this to cause player to lose however we want.
     {
         SceneManager.LoadScene(loseSceneID);
+    }
+
+    private void SetTimerText() // Format time into text, and print to string.
+    {
+        float localTimerTime = timer;
+
+        // Define various time elements.
+        int minutes = 0, seconds = 0;
+        while (localTimerTime > 60)
+        {
+            localTimerTime -= 60;
+            minutes++;
+        }
+        seconds = (int)localTimerTime;
+
+        string txt = $"Timer: {minutes}:{seconds}";
+
+        timerText.text = txt;
     }
 }
