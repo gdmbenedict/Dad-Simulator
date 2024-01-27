ec00ed6df99c76cc2f01a23478c71d76a67c6928
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-01-26 21:16:40 -0400

    Add rotation and -z motion.

--- Assets/MowerController.cs
+++ Assets/MowerController.cs
@@ -6,8 +6,8 @@ using UnityEngine.InputSystem;
 public class MowerController : MonoBehaviour
 {
     // Movement serialized backend
-    [SerializeField] float moveSpeed; // Move speed in m s -1
-    [SerializeField] float rotSpeed;
+    [SerializeField] float moveSpeed = 1; // Move speed in m s -1
+    [SerializeField] float rotSpeed = 30; // Rot speed in deg s -1
 
     // Movement backend things
     Vector2 moveValue;
@@ -24,7 +24,13 @@ public class MowerController : MonoBehaviour
     {
         // Movement things
         {
+            // Rotation
+            Vector3 rot = new Vector3(0, moveValue.x, 0) * rotSpeed * Time.deltaTime;
+            transform.Rotate(rot);
+
+            // Movement
             Vector3 moveVector = transform.forward * moveSpeed * Time.deltaTime;
+            if(moveValue.y < 0) { moveVector *= -0.5f; } // If we want to reverse, allow that, but make it slow.
             Vector3 movePos = moveVector + transform.position;
             rb.MovePosition(movePos);
         }
